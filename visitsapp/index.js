57a4fa9192f7c2004ddf855fa9c708f7bca16f20
Abdul Rahman <abdulnsamba@gmail.com>
2024-11-22 16:06:27 +0300

    update for redisv4

--- visitsapp/index.js
+++ visitsapp/index.js
@@ -6,20 +6,17 @@ const client = redis.createClient({
   url: "redis://redis-server:6379",
 });
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+(async () => {
+  await client.connect();
+  await client.set("visits", 0);
+})();
 
-client.connect().then(() => {
-  console.log("Connected to Redis.");
-  client.set("visits", 0);
+app.get("/", async (req, res) => {
+  const visits = await client.get("visits");
+  res.send("Number of visits " + visits);
+  await client.set("visits", parseInt(visits) + 1);
 });
 
-app.get("/", (req, res) => {
-  client.get("visits", (err, visits) => {
-    res.send(`This app has been visited ${visits} times.`);
-    client.set("visits", parseInt(visits) + 1);
-  });
-});
-
-app.listen(8080, (req, res) => {
-  console.log("Server started on port 8080..");
+app.listen(8081, () => {
+  console.log("Server started on port 8081..");
 });
